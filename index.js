b91a087056315c99a0a9310bfeb7583f2fecb397
Daniel Yakobian <daniel.yakobian@gotinder.com>
2017-07-26 23:49:21 -0700

    add index file

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const createSshKeys = require('./createSshKeys');
+const cleanupSsh = require('./cleanupSsh');
+const setupSsh = require('./setupSsh');
+
+module.exports = { createSshKeys, cleanupSsh, setupSsh};
