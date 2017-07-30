00457137e70bea767b4efdfde09fb403e5e93286
Daniel Yakobian <daniel.yakobian@gotinder.com>
2017-07-30 12:48:28 -0700

    make files executable

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 
 const {resolve} = require('path');
 const runScript = require('./runScript');
+const setExecutable = require('./setExecutable');
+
+setExecutable();
 
 const arg = process.argv.slice(2)[0];
 runScript(resolve(__dirname, `scripts/${arg}.sh`));
---
+++ setExecutable.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const { readdirSync } = require('fs');
+const { execSync } = require('child_process');
+
+function listBashFiles(p) {
+  return readdirSync(p);
+}
+
+function setExecutable() {
+  const scriptsPath = `${__dirname}/scripts`;
+  const bashFiles = listBashFiles(scriptsPath);
+  for (let file of bashFiles) {
+    const filePath = `${scriptsPath}/${file}`;
+    execSync(`chmod +x ${filePath}`);
+  }
+}
+
+module.exports = setExecutable;
